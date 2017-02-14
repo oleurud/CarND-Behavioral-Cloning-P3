50cde30e00283d6742eb2c1fb250c9fd905e86a5
Simon Martin <oleurud@gmail.com>
2017-02-14 20:31:48 +0100

    initial structure

---
+++ model.py
@@ -0,0 +1,107 @@
+import csv
+import cv2
+import numpy as np
+import sklearn
+from sklearn.model_selection import train_test_split
+
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten, Lambda, Cropping2D
+from keras.layers.convolutional import Convolution2D
+
+
+def get_data():
+    """
+    Read all the training data and returns train_samples and validation_samples
+    """
+
+    folders = [
+        './myData/center/',
+        './myData/clockwise/',
+        './myData/recovery/',
+        './myData/track2/',
+        './data/data/'
+    ]
+
+    samples = []
+
+    for folder in folders:
+        with open(folder + 'driving_log.csv') as csvfile:
+            reader = csv.reader(csvfile)
+            for line in reader:
+                samples.append(line)
+
+    return train_test_split(samples, test_size=0.2)
+
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while True: # Loop forever so the generator never terminates
+        samples = sklearn.utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+
+            images = []
+            angles = []
+
+            for batch_sample in batch_samples:
+                name = './IMG/'+batch_sample[0].split('/')[-1]
+                center_image = cv2.imread(name)
+                center_angle = float(batch_sample[3])
+                images.append(center_image)
+                angles.append(center_angle)
+
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield X_train, y_train
+
+
+def get_nvidia_model():
+
+    model = Sequential()
+    
+    row, col, ch = 160, 320, 3
+    model.add(Cropping2D(cropping=((50, 30), (0, 0)), input_shape=(row, col, ch)))
+
+    row, col, ch = 80, 320, 3
+    model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(row, col, ch), output_shape=(row, col, ch)))
+
+    model.add(Convolution2D(24, 5, 5, border_mode='valid', activation='relu', subsample=(2,2)))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(36, 5, 5, border_mode='valid', activation='relu', subsample=(2,2)))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(48, 5, 5, border_mode='valid', activation='relu', subsample=(2,2)))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(64, 3, 3, border_mode='valid', activation='relu', subsample=(1,1)))
+    model.add(Dropout(0.5))
+    model.add(Convolution2D(64, 3, 3, border_mode='valid', activation='relu', subsample=(1,1)))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dense(1164, activation="relu"))
+    model.add(Dense(100, activation="relu"))
+    model.add(Dense(50, activation="relu"))
+    model.add(Dense(10, activation="relu"))
+    model.add(Dense(1))
+
+    model.compile(loss='mse', optimizer='adam')
+
+    return model
+
+def training():
+    train_samples, validation_samples = get_data()
+    # compile and train the model using the generator function
+    train_generator = generator(train_samples)
+    validation_generator = generator(validation_samples)
+
+    model = get_nvidia_model()
+    model.fit_generator(
+        train_generator, 
+        samples_per_epoch=len(train_samples), 
+        validation_data=validation_generator,
+        nb_val_samples=len(validation_samples), 
+        nb_epoch=3
+    )
+    
+    model.save('model.h5')
+
+
+training()
